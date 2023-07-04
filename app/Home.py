b3cd1c085bd9e329b561b7bafca1f70bff4a5c82
darylkdps <darylkdps-github@yahoo.com>
2023-07-04 15:47:40 +0800

    First commit

---
+++ app/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Daryl's Research Page",
+    page_icon='📚',
+    layout='wide',
+    initial_sidebar_state='expanded'
+    )
+
+st.title("Daryl's Research Page")
